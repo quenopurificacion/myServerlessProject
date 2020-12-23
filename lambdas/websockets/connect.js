07280eee8f66d1f712a27ad9c514a1e7e1a94183
quenopurificacion <quian0108@gmail.com>
2020-12-23 16:38:10 +1300

    Updating websockets and serverless.yml

--- lambdas/websockets/connect.js
+++ lambdas/websockets/connect.js
@@ -1,8 +1,21 @@
 const Responses = require('../common/API_Responses')
+const Dynamo = require('../common/Dynamo');
+
+const tableName = process.env.tableName;
 
 exports.handler = async event => {
 
     console.log('event', event)
+
+    const { connectionId: connectionID } = event.requestContext;
+
+    const data = {
+        ID: connectionID,
+        date: Date.now(),
+        messages: []
+    }
+
+    await Dynamo.write(data, tableName)
 
     return Responses._200({message: 'connected'})
 }
--- lambdas/websockets/message.js
+++ lambdas/websockets/message.js
@@ -1,8 +1,33 @@
-const Responses = require('../common/API_Responses')
+const Responses = require('../common/API_Responses');
+const Dynamo = require('../common/Dynamo');
+const Dynamo = require('../common/Dynamo');
+const tableName = process.env.tableName;
 
 exports.handler = async event => {
 
     console.log('event', event)
 
-    return Responses._200({message: 'got a message'})
+    const { connectionId: connectionID } = event.requestContext;
+
+    const body = JSON.parse(event.body);
+
+    try {
+        const record = await Dynamo.get(connectionID, TableName)
+        const messages = record.messages;
+
+        messages.push(body.message);
+
+        const data = {
+            ...record,
+            messages
+        };
+
+        await Dynamo.write(data, tableName)
+
+        return Responses._200({message: 'got a message'})
+
+    } catch (error) {
+        return Responses._400({message: 'message could no be received'})
+    }
+    
 }
