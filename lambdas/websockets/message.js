a67c4602f655c60fc929e79954f49fb4bd76b2fe
quenopurificacion <quian0108@gmail.com>
2020-12-23 16:14:30 +1300

    Adding websockets

---
+++ lambdas/websockets/message.js
@@ -0,0 +1,8 @@
+const Responses = require('../common/API_Responses')
+
+exports.handler = async event => {
+
+    console.log('event', event)
+
+    return Responses._200({message: 'got a message'})
+}
